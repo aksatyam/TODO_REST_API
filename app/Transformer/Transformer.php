3719f9291ec659ca77c25502d5215e9251e54616
aksatyam <ashishsatyam.kmr@gmail.com>
2018-02-09 16:01:16 +0530

    Fist Commit

---
+++ app/Transformer/Transformer.php
@@ -0,0 +1,14 @@
+<?php namespace App\Transformer;
+
+use League\Fractal\TransformerAbstract;
+ 
+class TaskTransformer extends TransformerAbstract {
+ 
+    public function transform($task) {
+        return [
+            'id' => $task->id,
+            'task' => $task->name,
+            'task_description' => $task->description
+        ];
+    }
+ }
